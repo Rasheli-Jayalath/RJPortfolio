2844345cea3ba244bc526e2c85af6447668cda9c
Rasheli Jayalath <16.rasheli@gmail.com>
2025-03-10 15:25:44 +0200

    added files

---
+++ src/data/articles.js
@@ -0,0 +1,65 @@
+import React from "react";
+import INFO from "../data/user";
+
+function article_1() {
+	return {
+		date: "January 2025",
+		title: "Digitalization in University-Industry Collaboration",
+		description:
+			"Exploring how digital transformation reshapes academic-industry partnerships, balancing innovation, research, and industrial demands.",
+		keywords: [
+			"Digitalization in University-Industry Collaboration",
+			"Rashelli",
+			"Rasheli NJ",
+			"Rasheli Jayalath",
+		],
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<div className="paragraph">{INFO.articles.article1_intro}</div>
+					<a href="http://dx.doi.org/10.13140/RG.2.2.14911.21926" target="_blank" rel="noopener noreferrer">
+						<img
+							src="/ART1.png"
+							alt="article1"
+							className="artImage"
+						/>
+					</a>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
+		date: "January 2025",
+		title: "Createch for Small Business Productivity",
+		description:
+			"Examining how technology-driven solutions enhance efficiency, decision-making, and sustainability in small businesses.",
+		style: ``,
+		keywords: [
+			"Createch for Small Business Productivity",
+			"Rashelli",
+			"Rasheli NJ",
+			"Rasheli Jayalath",
+		],
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<div className="paragraph">{INFO.articles.article2_intro}</div>
+					<a href="http://dx.doi.org/10.13140/RG.2.2.26058.89288" target="_blank" rel="noopener noreferrer">
+						<img
+							src="/ART2.png"
+							alt="article2"
+							className="artImage"
+						/>
+					</a>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+const myArticles = [article_1, article_2];
+
+export default myArticles;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import Socials from "../components/about/socials";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/contact.css";
+
+const Contact = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "contact");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Contact | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="contact" />
+				<div className="content-wrapper">
+					<div className="contact-logo-container">
+						<div className="contact-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+
+					<div className="contact-container">
+						<div className="title contact-title">
+							Reach Out: I'm Always Open to Connect
+						</div>
+
+						<div className="subtitle contact-subtitle">
+						I appreciate your interest in connecting with me. Whether you have feedback, questions, or ideas, feel free to reach out. You can email me directly at
+							&nbsp;{" "}
+							<a href={`mailto:${INFO.main.email}`}>
+								{INFO.main.email}
+							</a>
+							, and I try to respond within 24 hours, though it may take a bit longer during busy times. If you'd rather use a contact form, you can find that on my website as well. I’m also active on social media, where I share updates and interact with my followers. Looking forward to hearing from you!
+						</div>
+					</div>
+
+					<div className="socials-container">
+						<div className="contact-socials">
+							<Socials />
+						</div>
+					</div>
+
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Contact;
