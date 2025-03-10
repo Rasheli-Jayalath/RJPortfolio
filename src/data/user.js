6737ca551b6cc8de0b677c7868ce5fa0297e6d5b
Rasheli Jayalath <16.rasheli@gmail.com>
2025-03-10 15:44:06 +0200

    added files

---
+++ src/data/user.js
@@ -0,0 +1,89 @@
+const INFO = {
+	main: {
+		title: "RJPortfolio",
+		name: "Rasheli.",
+		email: "jayalathrasheli@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Rasheli-Jayalath",
+		linkedin: "https://www.linkedin.com/in/edippulli-rasheli-jayalath-93b281198/",
+		researchgate: "https://www.researchgate.net/profile/Rasheli-Jayalath",
+
+	},
+
+	homepage: {
+		title: "MEng Candidate in Data-Centric Engineering |",
+		title1: "SE Background & Expertise |",
+		title2: "Academia Experience",
+		description:
+			" I am an MEng candidate in Data-Centric Engineering at LUT University, with a strong foundation in Software Engineering from my Bachelor's degree at the University of Westminster. I have worked in both industry and academia to gain diverse experience and perspectives in the field. In the industry, I worked as a Programmer at Surbana Jurong Group, contributing to software development and problem-solving. In academia, I served as an Assistant Lecturer at the Informatics Institute of Technology, where I conducted lectures and guided students in programming, project management, and related subjects. With experience in both domains, I bring a well-rounded understanding of software development, data-driven technologies, and teaching, driven by a passion for innovation and continuous learning.",
+	},
+
+	about: {
+		title: "I’m Rasheli, living in Finland, turning ideas into reality",
+		description:
+			" With expertise in React.js, React Native, PHP, Laravel, and database management, I specialize in crafting efficient, data-driven applications. My technical skill set includes full-stack development, RESTful API design, cloud integration, and software architecture. I have experience in Python and Java, particularly for backend development and automation. I am skilled in project management, agile methodologies, and optimizing code for performance and security. With strong problem-solving abilities and a passion for innovation, I continuously explore new technologies to enhance digital solutions. Many of my projects are open-source, and I welcome collaboration and fresh ideas. Let’s create something meaningful together!",
+	},
+
+	articles: {
+		title: "Pushing Boundaries and Inspiring the Next Generation",
+		description:
+			"A collection of my thoughts on programming, research, and education. After returning to academia as an assistant lecturer, my interest in research grew, allowing me to blend practical experience with new ideas to inspire and guide future innovators.",
+		article1_intro: 
+			"This study investigates the evolving dynamics of university-industry collaborations, focusing on the transformative impact of digitalization from an academic perspective. By exploring the interplay between academic research and industrial innovation, it highlights how digital transformation is reshaping these partnerships. The research examines the challenges and opportunities that arise when balancing academic goals with industrial demands in a rapidly digitalizing world. It also reflects on the structural and strategic shifts needed to ensure these collaborations remain sustainable, impactful, and future-ready. The study offers a framework for understanding how digitalization can act as a catalyst for innovation while addressing the complexities of modern collaboration.",
+		article2_intro: 
+			"The research focuses on exploring how Createch solutions can enhance productivity in small business organizations. It examines how technology-driven solutions like automation, cloud-based services, and analytics may help to streamline processes, improve decision-making, and boost overall efficiency. The study highlights the challenges faced by small businesses in adopting these solutions and offers practical strategies for successful implementation. The research also considers the sustainability impact of integrating innovative digital platforms.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/php/php.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/css/css.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+	],
+};
+
+export default INFO;
